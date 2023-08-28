9a938f313eb2b47dd376ef7b1c684fa9d5e67280
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2023-08-28 16:59:38 -0300

    Initial Commit

---
+++ MacOS Syncraft Installer for Cura/MacOS_Syncraft_Installer_for_CuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOS_Syncraft_Installer_for_CuraApp.swift
+//  MacOS Syncraft Installer for Cura
+//
+//  Created by Rafael Neuwirth Swierczynski on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MacOS_Syncraft_Installer_for_CuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
